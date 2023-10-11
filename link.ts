343273faca25a6f019b2d40c13596df6cf350371
chee <yay@chee.party>
2023-10-11 18:20:13 +0100

    0

--- link.ts
+++ link.ts
@@ -1,13 +1,16 @@
 Deno.serve({port: 51234}, request => {
-	let sessions: {[key: string]: string} = {}
+	let sessions: {[key: string]: WebSocket[]} = {}
 	if (request.headers.get("upgrade") != "websocket") {
 		return new Response(null, {status: 501})
 	}
 	let path = new URL(request.url).pathname
 	const {socket, response} = Deno.upgradeWebSocket(request)
 	socket.addEventListener("open", function (event) {
-		console.log(this, event)
+		console.log({this: this, event})
 		console.log("a client connected!")
+	})
+	socket.addEventListener("close", function (event) {
+		sessions[path] = sessions[path]
 	})
 	socket.addEventListener("message", function (event) {
 		if (event.data === "ping") {
